67cf3bf02979e5801c4c11db6969b1ab34632466
Marcos Candeia <marrcooos@gmail.com>
2023-04-30 17:51:43 -0300

    Add local registry and deco-sites registry
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- registries.ts
+++ registries.ts
@@ -1,5 +1,18 @@
 export default function () {
   return Promise.resolve({
+    "deco-sites": {
+      type: "http",
+      baseUrl: (alias: string) => {
+        const [deployment, ...urls] = alias.split(
+          "@",
+        );
+        return `https://deco-sites-${deployment}.deno.dev${urls.join("@")}`;
+      }, // `deco-sites.name-deployment@/url
+    },
+    "local": {
+      type: "http",
+      baseUrl: "http://localhost:8000",
+    },
     "deco": {
       org: "deco-sites",
       repo: "deco",
